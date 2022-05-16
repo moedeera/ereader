820197ca0072c64fd09417f0d9f4a7e755675d3b
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-16 16:49:43 -0600

    committ

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Homepage } from "./Pages/Homepage/Homepage";
+import { AboutPage } from "./Pages/AboutPage/AboutPage";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Footer } from "./Components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
