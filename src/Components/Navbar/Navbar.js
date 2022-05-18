d591ad255be3196d86d4c48bcbaac296f26d1974
moedeera <82620264+moedeera@users.noreply.github.com>
2022-05-18 09:22:48 -0600

    committ

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,32 @@
 import React from "react";
+import "./Navbar.css";
+import icon from "./ereader.png";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="container">
+      <div className="navbar-container">
+        <div className="logo">
+          <img src={icon} alt="" />
+          <h2>Librum-Reader</h2>
+        </div>
+        <div className="navbar-menu">
+          <ul>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Support</a>
+            </li>
+            <li>
+              <button className="btn btn-primary">Login</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Homepage.css";
+import image1 from "./showcase.svg";
 
 export const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="container">
+      <div className="homepage-container">
+        <div className="info-section">
+          <div className="info-section-text">
+            <h1>A Simple Effective Reader App</h1>
+            <p>
+              A clean and simple way to read ebooks on tablets, cellphones and
+              desktops without worrying about cross-platform synchronization
+            </p>
+          </div>
+          <div className="info-section-button">
+            <button className="btn btn-primary">Download now</button>
+          </div>
+        </div>
+        <div className="image">
+          <img src={image1} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
