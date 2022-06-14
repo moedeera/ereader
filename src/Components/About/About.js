00edb04db4816078fffff77f523060844b1ccf9f
moedeera <82620264+moedeera@users.noreply.github.com>
2022-06-14 00:45:07 -0600

    committ

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,45 +1,48 @@
 import React from "react";
 import "./About.css";
+import { Fade } from "react-reveal";
 
 export const About = () => {
   return (
     <div className="container" id="about">
-      <div className="about-container">
-        <h1
-          style={{
-            color: "white",
-          }}
-        >
-          A little info about ourselves
-        </h1>
-        <div className="about-top">
-          <div className="about-quote">
-            <h1>
-              "The more that you read, the more things you will know. The more
-              that you learn, the more places you’ll go.”
-            </h1>
+      <Fade right>
+        <div className="about-container">
+          <h1
+            style={{
+              color: "white",
+            }}
+          >
+            A little info about ourselves
+          </h1>
+          <div className="about-top">
+            <div className="about-quote">
+              <h1>
+                "The more that you read, the more things you will know. The more
+                that you learn, the more places you’ll go.”
+              </h1>
+            </div>
+            <div className="about-info">
+              {" "}
+              <p>
+                A simple idea started by two self taught developers residing on
+                two ends of the world in Germany and Canada. We aim to create a
+                free-to-use product that encourages reading again by making it
+                more easier Librum-Reader is available on all of the following
+                platforms{" "}
+              </p>
+            </div>
           </div>
-          <div className="about-info">
-            {" "}
-            <p>
-              A simple idea started by two self taught developers residing on
-              two ends of the world in Germany and Canada. We aim to create a
-              free-to-use product That encourages reading again by making it
-              more easier Librum-Reader is available on all of the following
-              platforms{" "}
-            </p>
-          </div>
-        </div>
 
-        <div className="about-platforms">
-          {/* <i className="fab fa-firefox"></i>
+          <div className="about-platforms">
+            {/* <i className="fab fa-firefox"></i>
           <i className="fab fa-chrome"></i> */}
-          <i className="fab fa-microsoft"></i>
-          <i className="fab fa-apple"></i>
-          <i className="fab fa-android"></i>
-          <i className="fab fa-linux"></i>
+            <i className="fab fa-microsoft"></i>
+            <i className="fab fa-apple"></i>
+            <i className="fab fa-android"></i>
+            <i className="fab fa-linux"></i>
+          </div>
         </div>
-      </div>
+      </Fade>
     </div>
   );
 };
--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Features.css";
+import { Fade } from "react-reveal";
 import icon1 from "./icon1.png";
 import icon2 from "./icon2.png";
 import icon3 from "./icon3.png";
@@ -19,21 +20,24 @@ export const Features = () => {
           you are interested in. Librum reader allows you to sync all the books
           you are interested in in one application for more convenient reads.
         </p>
-        <div className="feature-icons">
-          <div className="feature-icon">
-            <img src={icon1} alt="" />
-            <h5>Available on all devices</h5>
+        <Fade left>
+          <div className="feature-icons">
+            <div className="feature-icon">
+              <img src={icon1} alt="" />
+              <h5>Available on all devices</h5>
+            </div>
+            <div className="feature-icon">
+              <img src={icon2} alt="" />
+              <h5>Available on all platforms</h5>
+            </div>
+            <div className="feature-icon">
+              <img src={icon3} alt="" />
+              <h5>Offline or Online</h5>
+            </div>
           </div>
-          <div className="feature-icon">
-            <img src={icon2} alt="" />
-            <h5>Available on all platforms</h5>
-          </div>
-          <div className="feature-icon">
-            <img src={icon3} alt="" />
-            <h5>Offline or Online</h5>
-          </div>
-        </div>
+        </Fade>
       </div>
+
       <div className="features-lg-container">
         <h2>Features</h2>
 
@@ -42,61 +46,63 @@ export const Features = () => {
           you are interested in. Librum reader allows you to sync all the books
           you are interested in in one application for more convenient reads.
         </p>
-        <div className="features-slide">
-          <div className="slide-selection">
-            <div className="slide-selection-menu">
-              <div className="featured-selection">
-                <h3 onClick={() => setSLide(1)}>Cross device</h3>
+        <Fade left>
+          <div className="features-slide">
+            <div className="slide-selection">
+              <div className="slide-selection-menu">
+                <div className="featured-selection">
+                  <h3 onClick={() => setSLide(1)}>Cross device</h3>
+                </div>
+                <div className="featured-selection">
+                  <h3 onClick={() => setSLide(2)}>Cross platform</h3>
+                </div>
+                <div className="featured-selection">
+                  <h3 onClick={() => setSLide(3)}>Offline available</h3>
+                </div>
               </div>
-              <div className="featured-selection">
-                <h3 onClick={() => setSLide(2)}>Cross platform</h3>
-              </div>
-              <div className="featured-selection">
-                <h3 onClick={() => setSLide(3)}>Offline available</h3>
-              </div>
+              <div
+                className="slide-selection-bar"
+                style={
+                  slide === 1
+                    ? { transform: "translateX(0)" }
+                    : slide === 2
+                    ? { transform: "translateX(100%)" }
+                    : { transform: "translateX(200%)" }
+                }
+              ></div>
             </div>
-            <div
-              className="slide-selection-bar"
-              style={
-                slide === 1
-                  ? { transform: "translateX(0)" }
-                  : slide === 2
-                  ? { transform: "translateX(100%)" }
-                  : { transform: "translateX(200%)" }
-              }
-            ></div>
-          </div>
 
-          {slide === 1 && (
-            <div className="slide-image">
-              <p>
-                Whether its on your Laptop, Tablet or Cellphone. This app is
-                always ready to facilitating a good read. Start
-                cross-synchronization today once you download and install.
-              </p>
-              <img src={svg1} alt="" />
-            </div>
-          )}
-          {slide === 2 && (
-            <div className="slide-image">
-              <p>
-                Design for all Operating Systems, you don't have to worry about
-                what system you are on. We have a support network for Windows
-                10, Apple, Android and Linux.
-              </p>
-              <img src={svg2} alt="" style={{ height: "350px" }} />
-            </div>
-          )}
-          {slide === 3 && (
-            <div className="slide-image">
-              <p>
-                No need for constant updates or a live connection. While you
-                have to ability to cross synchronize online, its not required.
-              </p>
-              <img src={svg3} alt="" />
-            </div>
-          )}
-        </div>
+            {slide === 1 && (
+              <div className="slide-image">
+                <p>
+                  Whether its on your Laptop, Tablet or Cellphone. This app is
+                  always ready to facilitating a good read. Start
+                  cross-synchronization today once you download and install.
+                </p>
+                <img src={svg1} alt="" />
+              </div>
+            )}
+            {slide === 2 && (
+              <div className="slide-image">
+                <p>
+                  Design for all Operating Systems, you don't have to worry
+                  about what system you are on. We have a support network for
+                  Windows 10, Apple, Android and Linux.
+                </p>
+                <img src={svg2} alt="" style={{ height: "350px" }} />
+              </div>
+            )}
+            {slide === 3 && (
+              <div className="slide-image">
+                <p>
+                  No need for constant updates or a live connection. While you
+                  have to ability to cross synchronize online, its not required.
+                </p>
+                <img src={svg3} alt="" />
+              </div>
+            )}
+          </div>
+        </Fade>
       </div>
     </div>
   );
