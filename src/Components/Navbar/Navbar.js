00c5069d440558a5e8479fdf21714af82697407e
moedeera <82620264+moedeera@users.noreply.github.com>
2022-06-14 02:30:11 -0600

    committ

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,76 +8,86 @@ export const Navbar = () => {
   const [show, setShow] = useState(false);
   const [selected, setSelected] = useState(1);
   return (
-    <div className="container">
-      <div className="navbar-container">
-        <div className="logo">
-          <img src={icon} alt="" />
-          <h2>Librum-Reader</h2>
-        </div>
-        <div className="navbar-menu">
-          <ul>
-            <li>
-              <Link
-                to={"./"}
-                onClick={() => {
-                  setSelected(1);
-                }}
-              >
-                <p style={selected === 1 ? { color: "crimson" } : {}}>Home</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"./support"}
-                onClick={() => {
-                  setSelected(2);
-                }}
-              >
-                <p style={selected === 2 ? { color: "crimson" } : {}}>
-                  Support
-                </p>
-              </Link>
-            </li>
+    <div
+      style={
+        selected === 2
+          ? { backgroundColor: "#2892D7" }
+          : { backgroundColor: "white" }
+      }
+    >
+      <div className="container">
+        <div className="navbar-container">
+          <div className="logo">
+            <img src={icon} alt="" />
+            <h2>Librum-Reader</h2>
+          </div>
+          <div className="navbar-menu">
+            <ul>
+              <li>
+                <Link
+                  to={"./"}
+                  onClick={() => {
+                    setSelected(1);
+                  }}
+                >
+                  <p style={selected === 1 ? { color: "crimson" } : {}}>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"./support"}
+                  onClick={() => {
+                    setSelected(2);
+                  }}
+                >
+                  <p style={selected === 2 ? { color: "crimson" } : {}}>
+                    Support
+                  </p>
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                to={"./about"}
-                onClick={() => {
-                  setSelected(3);
-                }}
-              >
-                <p style={selected === 3 ? { color: "crimson" } : {}}>About</p>
-              </Link>
-            </li>
+              <li>
+                <Link
+                  to={"./about"}
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                >
+                  <p style={selected === 3 ? { color: "crimson" } : {}}>
+                    About
+                  </p>
+                </Link>
+              </li>
 
-            <li>
-              <Link
-                to={"./login"}
-                onClick={() => {
-                  setSelected(0);
-                }}
-              >
-                <button className="btn btn-primary">Login</button>
-              </Link>
-            </li>
-          </ul>
+              <li>
+                <Link
+                  to={"./login"}
+                  onClick={() => {
+                    setSelected(0);
+                  }}
+                >
+                  <button className="btn btn-primary">Login</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
+        <div className="navbar-container-sm">
+          <div className="logo">
+            <img src={icon} alt="" />
+            <h2>Librum-Reader</h2>
+          </div>
+          <div style={{ color: "black" }}>
+            <i
+              className="fas fa-bars fa-2x"
+              onClick={() => {
+                setShow(true);
+              }}
+            ></i>
+          </div>
+        </div>
+        {show && <Menu setShow={setShow} />}
       </div>
-      <div className="navbar-container-sm">
-        <div className="logo">
-          <img src={icon} alt="" />
-          <h2>Librum-Reader</h2>
-        </div>
-        <div style={{ color: "black" }}>
-          <i
-            className="fas fa-bars fa-2x"
-            onClick={() => {
-              setShow(true);
-            }}
-          ></i>
-        </div>
-      </div>
-      {show && <Menu setShow={setShow} />}
     </div>
   );
 };
--- src/Pages/Support/SupportPage.js
+++ src/Pages/Support/SupportPage.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./SupportPage.css";
 import pic1 from "./pic1.png";
 import pic2 from "./pic2.png";
 import pic3 from "./pic3.png";
+import Lottie from "lottie-web";
+import banner from "./stars.svg";
 
 export const SupportPage = () => {
+  const container = useRef(null);
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./SupportPageLottie.json"),
+    });
+  }, []);
+
   return (
     <div className="supportPage-container">
-      <section className="header">
-        <h1>Support </h1>
-        <p>Join our community, its free!</p>
-      </section>
+      <div className="header-container">
+        <section className="header">
+          <div className="header-info">
+            <div>
+              {" "}
+              <h1>Support </h1>
+              <p>
+                Home{" "}
+                <i
+                  className="fa fa-circle"
+                  style={{ fontSize: "12px", color: "purple" }}
+                ></i>{" "}
+                Support
+              </p>
+            </div>
+          </div>
+          <div className="header-image">
+            <img src={banner} alt="" />
+          </div>
+        </section>
+      </div>
+
       <section className="support-reasons">
         <div className="support">
           <div className="support-image">
