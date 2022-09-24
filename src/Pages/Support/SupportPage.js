71fc50a30c14ac925c2c14166f0354aec5e80c85
moedeera <82620264+moedeera@users.noreply.github.com>
2022-09-24 00:24:11 -0600

    committ

--- src/Pages/Support/SupportPage.js
+++ src/Pages/Support/SupportPage.js
@@ -9,6 +9,7 @@ import pic4 from "./pic4.svg";
 
 import Lottie from "lottie-web";
 import banner from "./stars.svg";
+import { Link } from "react-router-dom";
 
 export const SupportPage = ({ message, anchor, cards }) => {
   console.log(window.location.href);
@@ -39,7 +40,14 @@ export const SupportPage = ({ message, anchor, cards }) => {
                 <div className="support-ways-unit sd1" id="sd-1">
                   <div className="sw-image">
                     {" "}
-                    <img src={reason3} alt="" className="im1" />
+                    <a
+                      href={"https://www.patreon.com/librum_reader"}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {" "}
+                      <img src={reason3} alt="" className="im1" />
+                    </a>
                   </div>
                   <h2>Support us on Patreon</h2>
                   <p>
@@ -51,7 +59,14 @@ export const SupportPage = ({ message, anchor, cards }) => {
                 <div className="support-ways-unit">
                   <div className="sw-image">
                     {" "}
-                    <img src={reason1} alt="" />
+                    <a
+                      href={"https://github.com/Librum-Reader/Librum"}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {" "}
+                      <img src={reason1} alt="" className="im1" />
+                    </a>
                   </div>
                   <h2>Contribute</h2>
                   <p>
@@ -65,7 +80,9 @@ export const SupportPage = ({ message, anchor, cards }) => {
                 <div className="support-ways-unit">
                   <div className="sw-image">
                     {" "}
-                    <img src={reason2} alt="" />
+                    <Link to={"../login"}>
+                      <img src={reason2} alt="" />
+                    </Link>
                   </div>
 
                   <h2>Join the Community</h2>
